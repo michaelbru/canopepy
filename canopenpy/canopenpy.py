df6d62be0f0030546297433abb0196b67d1a1cb2
mike <michaelbruk@gmail.com>
2016-04-07 06:40:44 +0300

    editted

--- canopenpy/canopenpy.py
+++ canopenpy/canopenpy.py
@@ -148,7 +148,7 @@ class Kvaser(canlib.canlib):
             self.openedChannels.remove(ch)
 
 
-    def setCurrentChannel( self,ch ):
+    #def setCurrentChannel( self,ch ):
         '''if two or more channels had been opened then set one of those for read/write 
         caution : this procedure is thread unsafe'''
 
